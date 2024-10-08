45c86369867f7853c1a4bb5a72f6f7fb86294db2
i.nechakhin <i.nechakhin@g.nsu.ru>
2024-10-08 09:49:28 +0000

    Create simple working with API VK. May get info about any person use user_id.

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+import vk
+from vk.exceptions import VkAPIError
+
+con_info = open('connect_info.json', 'r')
+con_data = json.load(con_info)
+
+api = vk.API(access_token=con_data["access_token"], v=con_data["v"])
+user_id = int(input("Enter user_id: "))
+
+try:
+    user = api.users.get(user_ids=user_id, fields=['counters'])
+    print(user)
+except VkAPIError as e:
+    print(e.message)
