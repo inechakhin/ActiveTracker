e41ce21ecfbec14f0f9a5417667a68f99931855b
i.nechakhin <i.nechakhin@g.nsu.ru>
2024-10-26 12:18:30 +0000

    Create func convert_vk_url_to_vkid in utils.

--- features.py
+++ features.py
@@ -1,26 +1,21 @@
 import pandas as pd
 import json
 import vk
+from utils import convert_vk_url_to_vkid
 
 
 def get_vkid_from_vk_list(vk_list: list) -> list:
     vkid_list = []
     for vk_url in vk_list:
-        if "vk.com" in vk_url or "@" in vk_url:
-            vk_url = (
-                vk_url.replace("https://", "")
-                .replace("m.vk.com/id", "")
-                .replace("m.vk.com/", "")
-                .replace("vk.com/id", "")
-                .replace("vk.com/", "")
-            )
-            if vk_url[0] == "@":
-                vk_url = vk_url[1:]
-            vkid_list.append(vk_url)
+        vkid = convert_vk_url_to_vkid(vk_url)
+        if vkid:
+            vkid_list.append(vkid)
     return vkid_list
 
 
-def build_features_dataframe(api: vk.API, vkid_list: list, columns: list) -> pd.DataFrame:
+def build_features_dataframe(
+    api: vk.API, vkid_list: list, columns: list
+) -> pd.DataFrame:
     f_df = pd.DataFrame(columns=columns)
     for vkid in vkid_list:
         values = []
