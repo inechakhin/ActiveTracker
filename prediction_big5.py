f4ccd05a0dac78fb2326e94f4e43f173572f74bd
i.nechakhin <i.nechakhin@g.nsu.ru>
2024-10-27 12:25:43 +0000

    Add process user with gaps in futures. And refill dataset with features.

--- prediction_big5.py
+++ prediction_big5.py
@@ -1,22 +1,26 @@
 import json
 import vk
+import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from utils import convert_vk_url_to_vkid, get_features_by_vkid
+from features import fill_gaps
 
-def preprocess_input_data(input_data: list):
-    input_data = np.where(input_data == None, np.nan, input_data)
+def preprocess_input_data(input_data: list, features_df: pd.DataFrame):
+    id = input_data[0]
+    if not (features_df["id"] == id).any():
+        features_df = pd.concat([pd.DataFrame([input_data], columns=features_df.columns), features_df], ignore_index=True)
+        features_df = fill_gaps(features_df)
+    input_data = features_df.loc[features_df["id"] == id].values.tolist()
     # input_data = (input_data - np.mean(input_data)) / np.std(input_data)
-    input_data = input_data.reshape(1, -1)
-    return input_data
+    return [input_data[0][1:]], features_df
 
 
-def predict_big5(vk_url: str, api: vk.API, features: list, model: Sequential):
+def predict_big5(vk_url: str, api: vk.API, features_df: pd.DataFrame, model: Sequential):
     vkid = convert_vk_url_to_vkid(vk_url)
+    features = features_df.columns
     values = get_features_by_vkid(api, vkid, features)
-    #print(values)
-    values = preprocess_input_data(values[1:])
-    #print(values)
+    values, features_df = preprocess_input_data(values, features_df)
     pred = model.predict(values)
-    return pred
+    return pred, features_df
 
